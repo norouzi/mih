bd022a0f4e9bb3b6aada3baaafc8c0ba20eb9000
Mohammad Norouzi <mohammad.n@gmail.com>
2012-08-17 17:36:27 -0400

    linscan.h checked in.

---
+++ include/linscan.h
@@ -0,0 +1,6 @@
+void linscan_query(UINT32 *counter, UINT32 *res, UINT8 *codes, UINT8 *queries, int N, UINT32 NQ, int B, int R, \
+		   int dim1codes, int dim1queries);
+
+/* void batchquery(UINT32* counter, UINT32* results, UINT8* q, UINT32 numq, int _numthreads); */
+/* void* threadquery(void* ptr); */
+/* void query(int tid, UINT32* counter, UINT32* res, UINT8* queries, UINT32 numq); */
