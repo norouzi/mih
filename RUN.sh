ac6985aba19013a8f927ad891b043864fd1ac652
Mohammad Norouzi <mohammad.n@gmail.com>
2012-10-05 16:09:22 -0400

    Dynamic linking of libraries. Now RUN.sh supports experiments with 128 and 256-bit codes

--- RUN.sh
+++ RUN.sh
@@ -4,6 +4,12 @@ matlab_root_dir=
 # add matlab to the PATH if it is not already there
 # download the data, and create data/inria/ folder that includes ANN_SIFT1B folder and files
 
+# if number of bits (nb) is not provided from outside, set it to 64
+if [ -z "$nb" ]
+then
+    nb=64;
+fi
+
 
 # some sanity checks
 if [ "$matlab_root_dir" = "" ]
@@ -26,13 +32,13 @@ fi
 # compilation
 mkdir -p build
 cd build
-cmake .. -DMATLABROOT=$matlab_root_dir
+cmake ..
 make
 cd ..
 
 # creation of the binary codes
 # runs matlab, and then create_lsh_codes script inside it.
-matlab -nojvm -nodisplay -nosplash -r "nb=64;create_lsh_codes;quit"
+matlab -nojvm -nodisplay -nosplash -r "nb=$nb;create_lsh_codes;quit"
 if [ $? != 0 ]; then
     echo "Could not run matlab to create lsh codes... Aborting"
     exit 1
@@ -42,21 +48,61 @@ fi
 # binary codes (10K 100K 1M 2M 5M 10M 20M 100M 200M 500M 1B
 # items). Number of substrings to be used for each case is stored in a
 # bash array nch.
-nch[1]=5;
-nch[2]=4;
-nch[3]=4;
-nch[4]=3;
-nch[5]=3;
-nch[6]=3;
-nch[7]=3;
-nch[8]=2;
-nch[9]=2;
-nch[10]=2;
-nch[11]=2;
-nch[12]=2;
+if [ "$nb" = "64" ]
+then
+    nch[1]=5;
+    nch[2]=4;
+    nch[3]=4;
+    nch[4]=3;
+    nch[5]=3;
+    nch[6]=3;
+    nch[7]=3;
+    nch[8]=2;
+    nch[9]=2;
+    nch[10]=2;
+    nch[11]=2;
+    nch[12]=2;
+else
+    if [ "$nb" = "128" ]
+	nch[1]=10;
+	nch[2]=8;
+	nch[3]=8;
+	nch[4]=6;
+	nch[5]=6;
+	nch[6]=5;
+	nch[7]=5;
+	nch[8]=5;
+	nch[9]=5;
+	nch[10]=4;
+	nch[11]=4;
+	nch[12]=4;
+    else
+	if [ "$nb" = "256" ]
+	    nch[1]=19;
+	    nch[2]=15;
+	    nch[3]=13;
+	    nch[4]=12;
+	    nch[5]=11;
+	    nch[6]=11;
+	    nch[7]=10;
+	    nch[8]=10;
+	    nch[9]=10;
+	    nch[10]=9;
+	    nch[11]=9;
+	    nch[12]=8;
+	else
+	    echo "nb = $nb is not supported";
+	fi
+    fi
+fi
+
+db_file="codes/lsh/lsh_"$nb"_sift_1B.mat";
+mih_results="cache/mih_"$nb"_1B.mat";
+linscan_results="cache/linscan_"$nb"_1B.mat";
+
 for ((nm=1; nm<=12; nm=nm+1))
 do 
-    build/mih codes/lsh/lsh_64_sift_1B.mat cache/mih_64_1B.mat -Q 1000 -nMs 0.01 0.10 1 2 5 10 20 50 100 200 500 1000 -nM $nm -m ${nch[$nm]}
+    build/mih $db_file $mih_results  -Q 1000 -nMs 0.01 0.10 1 2 5 10 20 50 100 200 500 1000 -nM $nm -m ${nch[$nm]}
     if [ $? != 0 ]; then
 	echo "Could not run mih for some reason... Aborting"
 	exit 1
@@ -68,7 +114,7 @@ done
 # speed. The K in K-NN is set to 1000 (-K 1000)
 for ((nm=1; nm<=12; nm=nm+1))
 do
-    build/linscan codes/lsh/lsh_64_sift_1B.mat cache/linscan_64_1B.mat -Q 100 -nMs 0.01 0.10 1 2 5 10 20 50 100 200 500 1000 -nM $nm -K 1000
+    build/linscan $db_file $linscan_results -Q 100 -nMs 0.01 0.10 1 2 5 10 20 50 100 200 500 1000 -nM $nm -K 1000
     if [ $? != 0 ]; then
 	echo "Could not run linscan for some reason... Aborting"
 	exit 1
@@ -76,7 +122,7 @@ do
 done
 
 # Testing the results of mih with the results of linscan
-matlab -nojvm -nodisplay -nosplash -r "addpath('test');test_mih_with_linscan('codes/lsh/lsh_64_sift_1B.mat',8,'cache/mih_64_1B.mat','cache/linscan_64_1B.mat');quit"
+matlab -nojvm -nodisplay -nosplash -r "addpath('test');test_mih_with_linscan($db_file,8,$mih_results,$linscan_results');quit"
 if [ $? != 0 ]; then
     echo "Could not run test_mih_with_linscan for some reason... Aborting"
     exit 1
@@ -90,3 +136,15 @@ addpath matlab;
 plot_time('cache/mih_64_1B.mat', 'cache/linscan_64_1B.mat');
 plot_time('cache/mih_64_1B.mat', 'cache/linscan_64_1B.mat', [], 0, [.01 1000], [0 .2]);
 plot_time('cache/mih_64_1B.mat', 'cache/linscan_64_1B.mat', [], .0002, [.01 1000], [0 20]);
+
+
+addpath matlab;
+plot_time('cache/mih_mlh_128_1B.mat', 'cache/linscan_mlh_128_1B.mat');
+plot_time('cache/mih_mlh_128_1B.mat', 'cache/linscan_mlh_128_1B.mat', [], 0, [.01 1000], [0 1.1]);
+plot_time('cache/mih_mlh_128_1B.mat', 'cache/linscan_mlh_128_1B.mat', [], .0002, [.01 1000], [0 30]);
+
+
+addpath matlab;
+plot_time('cache/mih_256_1B.mat', 'cache/linscan_256_1B.mat');
+plot_time('cache/mih_256_1B.mat', 'cache/linscan_256_1B.mat', [], 0, [.01 1000], [0 1.1]);
+plot_time('cache/mih_256_1B.mat', 'cache/linscan_256_1B.mat', [], .0002, [.01 1000], [0 30]);
