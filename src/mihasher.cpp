52c46202ccb0e9045d0f7926bfe76ce6d40cf6e4
Mohammad Norouzi <mohammad.n@gmail.com>
2012-10-12 16:46:45 -0400

    rearranged the file, and added some comments

--- src/mihasher.cpp
+++ src/mihasher.cpp
@@ -3,67 +3,54 @@
 
 using namespace std;
 
-mihasher::mihasher(int _B, int _m)
+/*
+ * Inputs: query, numq, dim1queries
+ */
+
+/*
+ * Outputs: results, numres, stats
+ *
+ *   results: an array of indices (1-based) of the K-nearest neighbors
+ *   for each query. So the array includes K*numq uint32 integers.
+ *
+ *   numres: includes the number of database entries that fall at any
+ *   specific Hamming distance from the query until the K nearest
+ *   neighbors are reached. So from this array you can figure out the
+ *   Hamming distances of the K-nearest neighbors.
+ */
+
+void mihasher::batchquery(UINT32 *results, UINT32 *numres, qstat *stats, UINT8 *queries, UINT32 numq, int dim1queries)
 {
-    B = _B;
-    B_over_8 = B/8;
-    m = _m;
-    b = ceil((double)B/m);
- 
-    D = ceil(B/2.0);		// assuming that B/2 is large enough radius to include all of the k nearest neighbors
-    d = ceil((double)D/m);
-   
-    mplus = B - m * (b-1);
-    // mplus     is the number of chunks with b bits
-    // (m-mplus) is the number of chunks with (b-1) bits
+    counter = new bitarray;
+    counter->init(N);
 
-    xornum = new UINT32 [d+2];
-    xornum[0] = 0;
-    for (int i=0; i<=d; i++)
-	xornum[i+1] = xornum[i] + choose(b, i);
-    
-    H = new SparseHashtable[m];
-    // H[i].init might fail
-    for (int i=0; i<mplus; i++)
-	H[i].init(b);
-    for (int i=mplus; i<m; i++)
-	H[i].init(b-1);
-}
+    UINT32 *res  = new UINT32[K*(D+1)];
+    UINT64 *chunks = new UINT64[m];
 
-void mihasher::setK(int _K)
-{
-    K = _K;
-}
+    UINT32 *presults = results;
+    UINT32 *pnumres = numres;
+    qstat *pstats = stats;
+    UINT8 *pq = queries;
 
-mihasher::~mihasher()
-{
-    delete[] xornum;
-    delete[] H;
-}
+    for (int i=0; i<numq; i++) {
+	query(presults, pnumres, pstats, pq, chunks, res);
 
-void mihasher::populate(UINT8 *_codes, UINT32 _N, int dim1codes)
-{
-    N = _N;
-    codes = _codes;
-    UINT64 * chunks = new UINT64[m];
-
-    UINT8 * pcodes = codes;
-    for (UINT64 i=0; i<N; i++, pcodes += dim1codes) {
-	split(chunks, pcodes, m, mplus, b);
-
-	for (int k=0; k<m; k++)
-	    H[k].insert(chunks[k], i);
-
-	if (i % (int)ceil(N/1000) == 0) {
-	    printf("%.2f%%\r", (double)i/N * 100);
-	    fflush(stdout);
-	}
+	presults += K;
+	pnumres += B+1;
+	pstats ++;
+	pq += dim1queries;
     }
 	
+    delete [] res;
     delete [] chunks;
+
+    delete counter;
 }
 
-void mihasher::query(UINT32 *results, UINT32* numres, qstat *stats, UINT8 *query, UINT64 * chunks, UINT32 * res)
+// Temp variables: chunks, res -- we did not want to malloc inside
+// query, so these arrays are passed from outside
+
+void mihasher::query(UINT32 *results, UINT32* numres, qstat *stats, UINT8 *query, UINT64 *chunks, UINT32 *res)
 {
     UINT32 maxres = K ? K : N;			// if K == 0 that means we want everything to be processed.
 						// So maxres = N in that case. Otherwise K limits the results processed.
@@ -177,30 +164,63 @@ void mihasher::query(UINT32 *results, UINT32* numres, qstat *stats, UINT8 *query
     stats->numres = n;
 }
 
-void mihasher::batchquery(UINT32 *results, UINT32 *numres, qstat *stats, UINT8 *queries, UINT32 numq, int dim1queries)
+mihasher::mihasher(int _B, int _m)
 {
-    counter = new bitarray;
-    counter->init(N);
+    B = _B;
+    B_over_8 = B/8;
+    m = _m;
+    b = ceil((double)B/m);
+ 
+    D = ceil(B/2.0);		// assuming that B/2 is large enough radius to include all of the k nearest neighbors
+    d = ceil((double)D/m);
+   
+    mplus = B - m * (b-1);
+    // mplus     is the number of chunks with b bits
+    // (m-mplus) is the number of chunks with (b-1) bits
 
-    UINT32 *res  = new UINT32[K*(D+1)];
-    UINT64 *chunks = new UINT64[m];
+    xornum = new UINT32 [d+2];
+    xornum[0] = 0;
+    for (int i=0; i<=d; i++)
+	xornum[i+1] = xornum[i] + choose(b, i);
+    
+    H = new SparseHashtable[m];
+    // H[i].init might fail
+    for (int i=0; i<mplus; i++)
+	H[i].init(b);
+    for (int i=mplus; i<m; i++)
+	H[i].init(b-1);
+}
 
-    UINT32 *presults = results;
-    UINT32 *pnumres = numres;
-    qstat *pstats = stats;
-    UINT8 *pq = queries;
+void mihasher::setK(int _K)
+{
+    K = _K;
+}
 
-    for (int i=0; i<numq; i++) {
-	query(presults, pnumres, pstats, pq, chunks, res);
+mihasher::~mihasher()
+{
+    delete[] xornum;
+    delete[] H;
+}
 
-	presults += K;
-	pnumres += B+1;
-	pstats ++;
-	pq += dim1queries;
+void mihasher::populate(UINT8 *_codes, UINT32 _N, int dim1codes)
+{
+    N = _N;
+    codes = _codes;
+    UINT64 * chunks = new UINT64[m];
+
+    UINT8 * pcodes = codes;
+    for (UINT64 i=0; i<N; i++, pcodes += dim1codes) {
+	split(chunks, pcodes, m, mplus, b);
+
+	for (int k=0; k<m; k++)
+	    H[k].insert(chunks[k], i);
+
+	if (i % (int)ceil(N/1000) == 0) {
+	    printf("%.2f%%\r", (double)i/N * 100);
+	    fflush(stdout);
+	}
     }
 	
-    delete [] res;
     delete [] chunks;
-
-    delete counter;
 }
+
