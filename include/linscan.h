b5908b4470cb7b87b346459bf9b042a225d64a7f
Mohammad Norouzi <mohammad.n@gmail.com>
2015-03-09 19:39:54 -0400

    minor update

--- include/linscan.h
+++ include/linscan.h
@@ -1,6 +1,7 @@
-void linscan_query(UINT32 *counter, UINT32 *res, UINT8 *codes, UINT8 *queries, int N, UINT32 NQ, int B, int R, \
-		   int dim1codes, int dim1queries);
+void linscan_query(UINT32 *counter, UINT32 *res, UINT8 *codes, UINT8 *queries, 
+                   int N, UINT32 NQ, int B, unsigned int K, int dim1codes, 
+                   int dim1queries);
 
-/* void batchquery(UINT32* counter, UINT32* results, UINT8* q, UINT32 numq, int _numthreads); */
-/* void* threadquery(void* ptr); */
-/* void query(int tid, UINT32* counter, UINT32* res, UINT8* queries, UINT32 numq); */
+void linscan_AH_query(REAL *dists, UINT32 *res, UINT8 *codes, REAL *centers,
+                      REAL *queries, int N, UINT32 NQ, int B, int K,
+                      int dim1codes, int dim1queries, int subdim);
